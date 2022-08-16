1439d6e9448611277507e65b16bedaa4526499e8
up1 <somkiat.p@gmail.com>
2022-08-16 14:30:47 +0700

    Create model of message

---
+++ workshop20220816/workshop01/src/main/java/com/example/workshop01/Employee.java
@@ -0,0 +1,31 @@
+package com.example.workshop01;
+
+public class Employee {
+
+    private int id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
