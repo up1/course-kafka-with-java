df9427614b9fb60d4b7e2f62690a205fdb0c9748
up1 <somkiat.p@gmail.com>
2022-08-16 13:57:01 +0700

    Creare workshop and configuration for kafka

---
+++ workshop20220816/workshop01/src/main/java/com/example/workshop01/Workshop01Application.java
@@ -0,0 +1,13 @@
+package com.example.workshop01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop01Application.class, args);
+	}
+
+}
---
+++ workshop20220816/workshop01/src/test/java/com/example/workshop01/Workshop01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
