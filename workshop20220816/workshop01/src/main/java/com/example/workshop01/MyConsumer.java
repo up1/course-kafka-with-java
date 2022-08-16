282a8f9271b910726d29ae8bfa3970c7b3f5d22c
up1 <somkiat.p@gmail.com>
2022-08-16 14:45:47 +0700

    Working with JSON serialization and deserialization

--- workshop20220816/workshop01/src/main/java/com/example/workshop01/MyConsumer.java
+++ workshop20220816/workshop01/src/main/java/com/example/workshop01/MyConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyConsumer {
 
-    @KafkaListener(topics = "demo.1.topic")
+    @KafkaListener(topics = "demo.2.topic")
     public void receiveData(Employee employee) {
         System.out.println("Received message = " + employee);
     }
